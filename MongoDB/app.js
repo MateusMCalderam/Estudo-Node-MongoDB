7c3f318aebd60bb1c70383d732d79f135a24fbd6
Mateus Maschio Calderam <125042084+MateusMCalderam@users.noreply.github.com>
2023-07-25 22:14:03 -0300

    Estudo MongoDB

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -3,15 +3,62 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/blogs')
-  .then((connection) => {
+
+//Conectar com o banco de dados
+mongoose.connect('mongodb://127.0.0.1:27017/pictures')
+  .then(() => {
     console.log('Conexão bem-sucedida com o MongoDB!');
-    console.log(connection);
   })
   .catch((error) => {
     console.error('Erro ao conectar ao MongoDB:', error);
   });
 
+
+//Cria um Esquema de como os dados devem ser enviados e quais são obrigatórios  
+  const docSchema = new mongoose.Schema({
+    title: String,
+    description: {type:String, required: true},
+    urlPicture: {type:String, required: true} 
+  })
+
+//Faz uma conexão do Esquema com o Modelo  
+  const Doc = mongoose.model('Doc', docSchema)
+
+//Cria um novo Documento chamado doc que será enviado de acordo com o modelo Doc
+  const doc = new Doc({
+    title: "Imagem2",
+    description: "Esse é a descrição da imagem 2",
+    urlPicture: "https://mundoconectado.com.br/uploads/2022/05/25/25658/cacto.jpg"
+  })
+
+//Salva o documento no Banco de Dados  
+  doc.save().then(doc =>{
+    console.log(doc)
+  }).catch( err => {
+    console.log(err)
+  })
+
+let db = mongoose.connection
+
+
+//Dentro do Banco Aberto, Ele pega os documentos com title, cria uma variavel com o title do documento, depois ele busca os documentos com title e coloca o resultado
+//dentro da variavel doc e redireciona para a url que tinha no documento.
+db.once("open", () => {
+  console.log("Banco Carregado");
+
+  app.get('/:title', async (req,res) => {
+    let title = req.params.title
+
+    try{
+    let doc = await Doc.findOne({title})
+
+      res.redirect(doc.urlPicture)
+   } catch (error){
+    res.send(error)  
+  }
+  })
+
+  })
 
 app.get('/', (req,res) => res.send("Hello World"))
 
